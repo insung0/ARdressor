b0c6e28d148b2c18b9791367520a8be433b886d2
insung0 <34765141+insung0@users.noreply.github.com>
2019-09-11 20:20:32 +0900

    Update script.js

--- script.js
+++ script.js
@@ -1,30 +1,26 @@
 const video = document.getElementById('video')
 var clothes = new Image()
 clothes.src = 'bustier.png'
-// const bustier = document.getElementById('bustier')
 
 
 Promise.all([
   faceapi.nets.tinyFaceDetector.loadFromUri('/models'),
-  faceapi.nets.faceLandmark68Net.loadFromUri('/models'),
-  faceapi.nets.faceRecognitionNet.loadFromUri('/models'),
-  faceapi.nets.faceExpressionNet.loadFromUri('/models')
 ]).then(startVideo)
 
 function startVideo() {
   navigator.getUserMedia(
-    { video: {} },
+    { video: true },
     stream => video.srcObject = stream,
     err => console.error(err)
   )
 }
+
 
 video.addEventListener('play', () => {
   const canvas = faceapi.createCanvasFromMedia(video)
   document.body.append(canvas)
   const displaySize = { width: video.width, height: video.height }
   faceapi.matchDimensions(canvas, displaySize)
-  // faceapi.draw(bustier)
   setInterval(async () => {
     const detections = await faceapi.detectSingleFace(video, new faceapi.TinyFaceDetectorOptions())
     const resizedDetections = faceapi.resizeResults(detections, displaySize)
@@ -32,9 +28,5 @@ video.addEventListener('play', () => {
     const box = resizedDetections.box
     
     canvas.getContext('2d').drawImage(clothes, box.x + box.width/2 - clothes.width/2, box.y + box.height)
-    drawBox.draw(canvas)
-    // canvas.drawImage(bustier, resizedDetections[0].x, resizedDetections[0].y)
-    // faceapi.draw.drawDetections(canvas, resizedDetections)
-    
   }, 100)
 })
